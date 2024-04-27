55969379a561cf5c0668e041700271e6c73fdea4
diondokter@p1pi <diondokter@gmail.com>
2024-04-28 00:19:01 +0100

    Improve timestamping

--- reader/src/main.rs
+++ reader/src/main.rs
@@ -1,7 +1,8 @@
-use std::io::Read;
 use std::time::Duration;
+use std::{error::Error, io::Read};
 
-use dsmr5::Tariff;
+use chrono::{TimeZone, Utc};
+use dsmr5::{Tariff, Telegram};
 
 fn main() {
     let port = serialport::new("/dev/ttyUSB0", 115_200)
@@ -30,50 +31,86 @@ fn main() {
 
         println!("\n---------------------------\n");
 
-        let mut data = Data::default();
-
-        for obj in telegram.objects() {
-            match obj {
-                Ok(obj) => match obj {
-                    dsmr5::OBIS::MeterReadingTo(Tariff::Tariff1, ref val) => {
-                        data.kwh_import_total_tarif_low = val.into()
-                    }
-                    dsmr5::OBIS::MeterReadingTo(Tariff::Tariff2, ref val) => {
-                        data.kwh_import_total_tarif_high = val.into()
-                    }
-                    dsmr5::OBIS::MeterReadingBy(Tariff::Tariff1, ref val) => {
-                        data.kwh_export_total_tarif_low = val.into()
-                    }
-                    dsmr5::OBIS::MeterReadingBy(Tariff::Tariff2, ref val) => {
-                        data.kwh_export_total_tarif_high = val.into()
-                    }
-                    dsmr5::OBIS::InstantaneousVoltage(line, ref val) => {
-                        data.voltages[line as usize] = val.into()
-                    }
-                    dsmr5::OBIS::InstantaneousActivePowerPlus(line, ref val) => {
-                        data.active_powers_import[line as usize] = val.into()
-                    }
-                    dsmr5::OBIS::InstantaneousActivePowerNeg(line, ref val) => {
-                        data.active_powers_export[line as usize] = val.into()
-                    }
-                    dsmr5::OBIS::SlaveMeterReading(_, timestamp, Some(ref val)) => {
-                        data.gas_meter = val.into();
-                        data.gas_meter_time = timestamp.second as u32
-                            + timestamp.minute as u32 * 60
-                            + (timestamp.hour as u32 + timestamp.dst as u32) * 3600;
-                    }
-                    _ => {}
-                },
-                Err(e) => println!("Obj error: {e:?}"),
+        let (electricity_data, slave_data) = match telegram_to_data(telegram) {
+            Ok(val) => val,
+            Err(e) => {
+                println!("Getting data error: {e:?}");
+                continue;
             }
-        }
+        };
 
-        println!("{data:#?}");
+        println!("{electricity_data:#?}");
+        println!("{slave_data:#?}");
     }
 }
 
+fn telegram_to_data(
+    telegram: Telegram,
+) -> Result<(ElectricityData, [Option<SlaveData>; 4]), Box<dyn Error>> {
+    let mut electricity_data = ElectricityData {
+        time: chrono::Utc::now(),
+        ..Default::default()
+    };
+    let mut slave_data = [None; 4];
+
+    for obj in telegram.objects() {
+        match obj {
+            Ok(obj) => match obj {
+                dsmr5::OBIS::MeterReadingTo(Tariff::Tariff1, ref val) => {
+                    electricity_data.kwh_import_total_tarif_low = val.into()
+                }
+                dsmr5::OBIS::MeterReadingTo(Tariff::Tariff2, ref val) => {
+                    electricity_data.kwh_import_total_tarif_high = val.into()
+                }
+                dsmr5::OBIS::MeterReadingBy(Tariff::Tariff1, ref val) => {
+                    electricity_data.kwh_export_total_tarif_low = val.into()
+                }
+                dsmr5::OBIS::MeterReadingBy(Tariff::Tariff2, ref val) => {
+                    electricity_data.kwh_export_total_tarif_high = val.into()
+                }
+                dsmr5::OBIS::InstantaneousVoltage(line, ref val) => {
+                    electricity_data.voltages[line as usize] = val.into()
+                }
+                dsmr5::OBIS::InstantaneousActivePowerPlus(line, ref val) => {
+                    electricity_data.active_powers_import[line as usize] = val.into()
+                }
+                dsmr5::OBIS::InstantaneousActivePowerNeg(line, ref val) => {
+                    electricity_data.active_powers_export[line as usize] = val.into()
+                }
+                dsmr5::OBIS::SlaveMeterReading(s, timestamp, Some(ref val)) => {
+                    let offset =
+                        chrono::FixedOffset::east_opt(if timestamp.dst { 2 } else { 1 } * 3600)
+                            .ok_or_else(|| format!("Timezone error!: {timestamp:?}"))?;
+
+                    slave_data[s as usize] = Some(SlaveData {
+                        time: offset
+                            .with_ymd_and_hms(
+                                timestamp.year as i32 + 2000,
+                                timestamp.month as u32,
+                                timestamp.day as u32,
+                                timestamp.hour as u32,
+                                timestamp.minute as u32,
+                                timestamp.second as u32,
+                            )
+                            .latest()
+                            .ok_or_else(|| format!("Time error!: {timestamp:?}"))?
+                            .to_utc(),
+                        value: val.into(),
+                    });
+                }
+                _ => {}
+            },
+            Err(e) => println!("Obj error: {e:?}"),
+        }
+    }
+
+    Ok((electricity_data, slave_data))
+}
+
 #[derive(Debug, Default)]
-struct Data {
+struct ElectricityData {
+    time: chrono::DateTime<Utc>,
+
     kwh_import_total_tarif_low: f64,
     kwh_import_total_tarif_high: f64,
     kwh_export_total_tarif_low: f64,
@@ -82,7 +119,10 @@ struct Data {
     voltages: [f64; 3],
     active_powers_import: [f64; 3],
     active_powers_export: [f64; 3],
+}
 
-    gas_meter: f64,
-    gas_meter_time: u32,
+#[derive(Debug, Copy, Clone, Default)]
+struct SlaveData {
+    time: chrono::DateTime<Utc>,
+    value: f64,
 }
