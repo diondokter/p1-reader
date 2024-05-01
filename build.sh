cf91612433263bb59a0c2e57d1fd359a505725ac
diondokter@p1pi <diondokter@gmail.com>
2024-05-01 22:22:34 +0100

    log build status

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+echo "Starting database"
 docker compose up db -d --wait
 cd reader
+echo "Running migrations"
 sqlx migrate run
 cd ../
+echo "Building docker"
 docker compose build
+echo "Stopping database"
 docker compose down
